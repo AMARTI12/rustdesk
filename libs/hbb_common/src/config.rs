476d19e7fae8f4d741f3472a2ed6d44e643e8f09
Fredy Alexander Martinez <freddyalex1272.fm@gmail.com>
2022-12-15 14:19:45 -0500

    change server

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -78,11 +78,9 @@ const CHARS: &'static [char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &'static [&'static str] = &[
-    "rs-ny.rustdesk.com",
-    "rs-sg.rustdesk.com",
-    "rs-cn.rustdesk.com",
+    "remotely.excellentiam.co",
 ];
-pub const RS_PUB_KEY: &'static str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RS_PUB_KEY: &'static str = "tCRXNQmdNVnGLLBFg8AY+BASxAM9AGw1r2Zps8FRlHU=";
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
 
